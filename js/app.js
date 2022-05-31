ebadb53d56594f8a6cfd67e0815e3f7c5f87b278
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-05-31 13:41:15 +0300

    good and easy lib, made my own animation and tested native animations

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 import { isWebp } from './components/isWebp.js'
+import AOS from 'aos'
 
 isWebp()
 
+AOS.init({
+	disable: 'mobile',
+	startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+	offset: 150,
+	duration: 400, // values from 0 to 3000, with step 50ms
+	easing: 'ease', // default easing for AOS animations
+	once: false, // whether animation should happen only once - while scrolling down
+	anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+})
